b93fa08ae609e3bd862003ba64418687ff9db818
tajwarsaiyeed <rjabid36@gmail.com>
2025-01-19 12:45:06 +0600

    Update settings.py to configure ALLOWED_HOSTS and CSRF_TRUSTED_ORIGINS for deployment

--- LMS/settings.py
+++ LMS/settings.py
@@ -9,8 +9,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 DEBUG = True
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["lms-wsfd.onrender.com", "127.0.0.1"]
+CSRF_TRUSTED_ORIGINS=["https://lms-wsfd.onrender.com"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
